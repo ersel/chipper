20835c1053ecaa9e47edb79007bd4f906ed5b762
Ersel Aker <erselaker@gmail.com>
2019-09-08 18:46:49 +0100

    cjs support...

--- utils/static-require-resolver/index.js
+++ utils/static-require-resolver/index.js
@@ -2,9 +2,18 @@ const acorn = require('acorn');
 const injectJSX = require('acorn-jsx/inject');
 const injectObjectSpread = require('acorn5-object-spread/inject');
 const checkStaticRequireWithMemberExpressionsRecursively = require('./checkMemberImports');
+const parseIdentifiers = require('./parseIdentifiers');
 
 injectJSX(acorn);
 injectObjectSpread(acorn);
+
+const checkifVariableDeclaratorWithFunctionCall = node =>
+	node &&
+	node.declarations &&
+	node.declarations[0] &&
+	node.declarations[0].type === 'VariableDeclarator' &&
+	node.declarations[0].init &&
+	node.declarations[0].init.type === 'CallExpression';
 
 const checkIfDefaultRequire = node =>
 	node &&
@@ -45,46 +54,30 @@ const resolver = (moduleContents, fileName) => {
 	const nodes = ast.body;
 	for (let j = 0; j < nodes.length; j += 1) {
 		const node = nodes[j];
-		//
+		let memberImport;
+		let source;
 
-		console.log(
-			checkStaticRequireWithMemberExpressionsRecursively(
-				node.declarations[0].init
-			),
-			'what'
-		);
-		if (checkIfDefaultRequire(node)) {
-			const source = node.declarations[0].init.arguments[0].value;
-
-			console.log('is default require');
-			/*
-            dependencies.push({
-                source: '',
-                imports: []
-            });
-            */
-		}
-		/*
-			const imports = node.specifiers.map(n => {
-				if (n.type === 'ImportSpecifier') {
-					return {
-						imported: n.imported.name,
-						local: n.local.name
-					};
-				} else if (n.type === 'ImportNamespaceSpecifier') {
-					return {
-						imported: '*',
-						local: n.local.name
-					};
-				} else if (n.type === 'ImportDefaultSpecifier') {
-					return {
-						imported: 'default',
-						local: n.local.name
-					};
+		if (checkifVariableDeclaratorWithFunctionCall(node)) {
+			if (checkIfDefaultRequire(node)) {
+				source = {
+					source: node.declarations[0].init.arguments[0].value,
+					path: ''
+				};
+			} else {
+				memberImport = checkStaticRequireWithMemberExpressionsRecursively(
+					node.declarations[0].init
+				);
+				if (memberImport !== 'n/a') {
+					// todo: prepend . to path if it doesnt start with(
+					source = memberImport;
 				}
-				return 'Unsupported Export';
-            });
-            */
+			}
+		}
+
+		if (source) {
+			const imports = parseIdentifiers(node.declarations[0].id);
+			dependencies.push({ ...source, imports, type: 'commonjs' });
+		}
 	}
 
 	return dependencies;
