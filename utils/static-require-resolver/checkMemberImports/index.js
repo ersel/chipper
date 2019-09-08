019b28c15dbe16e5f9c42503d682d3775afec776
Ersel Aker <erselaker@gmail.com>
2019-09-08 17:34:13 +0100

    almost there...

--- utils/static-require-resolver/checkMemberImports/index.js
+++ utils/static-require-resolver/checkMemberImports/index.js
@@ -80,7 +80,6 @@ const checkStaticRequireWithMemberExpressionsRecursively = node => {
 		node.callee.type === 'MemberExpression'
 	) {
 		// keep diving until you can find a call expression
-		console.log(node);
 		const required = {
 			path: '()'
 		};
@@ -97,6 +96,73 @@ const checkStaticRequireWithMemberExpressionsRecursively = node => {
 		return results;
 	}
 
+	if (
+		node &&
+		node.type === 'MemberExpression' &&
+		node.object &&
+		node.object.type &&
+		node.object.type === 'CallExpression' &&
+		node.property &&
+		node.property.type &&
+		node.property.type === 'Identifier'
+	) {
+		// keep diving until you can find a call expression
+		const required = {
+			path: node.property.name
+		};
+		const results = checkStaticRequireWithMemberExpressionsRecursively(
+			node.object
+		);
+
+		if (results !== 'n/a') {
+			return {
+				path: `${results.path}${required.path}`,
+				source: results.source
+			};
+		}
+
+		return results;
+	}
+
+	if (
+		node &&
+		node.type === 'CallExpression' &&
+		node.callee &&
+		node.callee.type &&
+		node.callee.type === 'CallExpression'
+	) {
+		// keep diving until you can find a call expression
+		const required = {
+			path: `()`
+		};
+		const results = checkStaticRequireWithMemberExpressionsRecursively(
+			node.callee
+		);
+
+		if (results !== 'n/a') {
+			return {
+				path: `${results.path}${required.path}`,
+				source: results.source
+			};
+		}
+
+		return results;
+	}
+
+	if (
+		node &&
+		node.type === 'CallExpression' &&
+		node.callee &&
+		node.callee.type &&
+		node.callee.type === 'Identifier' &&
+		node.callee.name === 'require'
+	) {
+		return {
+			path: '',
+			source: node.arguments[0].value
+		};
+	}
+	console.log(node);
 	return 'n/a';
 };
 
